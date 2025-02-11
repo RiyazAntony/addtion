6e87685cbdb346d2ccc66fe77bf1eccfd069496a
RiyazAntony <antonyriyas18@gmail.com>
2025-02-11 15:19:20 +0530

    Add files via upload

---
+++ AddtionofTwo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.addtionoftwo;
+
+/**
+ *
+ * @author pacha
+ */
+public class AddtionofTwo {
+
+    public static void main(String[] args) {
+      int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+    }
+}
